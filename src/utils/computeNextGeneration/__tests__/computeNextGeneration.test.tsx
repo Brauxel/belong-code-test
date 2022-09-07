9505d2974af4dbcd6af18bfc29226c093265d9f3
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-07 21:44:43 +1000

    [Aakash Bhatia]-[Next Generation Computation]-[Write tests for generation 2 in screenshot]

--- src/utils/computeNextGeneration/__tests__/computeNextGeneration.test.tsx
+++ src/utils/computeNextGeneration/__tests__/computeNextGeneration.test.tsx
@@ -12,16 +12,31 @@ describe('computeNextGeneration', () => {
     ],
   }
 
+  const firstGeneration: CellValuesState = {
+    liveCells: [
+      [2, 1],
+      [2, 3],
+      [3, 2],
+      [3, 3],
+      [4, 2],
+    ],
+  }
+
+  const secondGeneration: CellValuesState = {
+    liveCells: [
+      [2, 3],
+      [3, 1],
+      [3, 3],
+      [4, 2],
+      [4, 3],
+    ],
+  }
+
   it('should correctly generate the first generation', () => {
-    const firstGeneration: CellValuesState = {
-      liveCells: [
-        [2, 1],
-        [2, 3],
-        [3, 2],
-        [3, 3],
-        [4, 2],
-      ],
-    }
     expect(computeNextGeneration(cellValues)).toEqual(firstGeneration)
+  })
+
+  it('should correctly generate the second generation', () => {
+    expect(computeNextGeneration(firstGeneration)).toEqual(secondGeneration)
   })
 })
