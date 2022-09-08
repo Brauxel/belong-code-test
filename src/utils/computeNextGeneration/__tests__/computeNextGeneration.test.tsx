c4cf652960495464e19be1b5cb5fe31b09b1ca5d
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-08 14:11:21 +1000

    [Aakash Bhatia]-[Next Generation Computation]-[tests expanded for more scenarios]

--- src/utils/computeNextGeneration/__tests__/computeNextGeneration.test.tsx
+++ src/utils/computeNextGeneration/__tests__/computeNextGeneration.test.tsx
@@ -2,64 +2,150 @@ import { computeNextGeneration } from '..'
 import { cellRange } from '../../../components/shared/elements/CellSimulator'
 
 describe('computeNextGeneration', () => {
-  const initialCellValues: cellRange[][] = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-  ]
+  describe('test provided scenarios', () => {
+    const initialCellValues: cellRange[][] = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]
 
-  const firstGeneration: cellRange[][] = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 1, 1, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-  ]
+    const firstGeneration: cellRange[][] = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]
 
-  const secondGeneration: cellRange[][] = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-  ]
+    const secondGeneration: cellRange[][] = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 1, 0, 1, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]
 
-  const thirdGeneration: cellRange[][] = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0],
-    [0, 0, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-  ]
+    const thirdGeneration: cellRange[][] = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 1, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]
 
-  const fourthGeneration: cellRange[][] = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 1, 0],
-    [0, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0],
-  ]
+    const fourthGeneration: cellRange[][] = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 1, 0],
+      [0, 0, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]
 
-  it('should correctly generate the first generation', () => {
-    expect(computeNextGeneration(initialCellValues)).toEqual(firstGeneration)
+    it('should correctly generate the first generation', () => {
+      expect(computeNextGeneration(initialCellValues)).toEqual(firstGeneration)
+    })
+
+    it('should correctly generate the second generation', () => {
+      expect(computeNextGeneration(firstGeneration)).toEqual(secondGeneration)
+    })
+
+    it('should correctly generate the third generation', () => {
+      expect(computeNextGeneration(secondGeneration)).toEqual(thirdGeneration)
+    })
+
+    it('should correctly generate the fourth generation', () => {
+      expect(computeNextGeneration(thirdGeneration)).toEqual(fourthGeneration)
+    })
   })
 
-  it('should correctly generate the second generation', () => {
-    expect(computeNextGeneration(firstGeneration)).toEqual(secondGeneration)
-  })
+  describe('test provided conditions', () => {
+    it('a cell with fewer than two live neighbors dies of under-population. ', () => {
+      const cellValues: cellRange[][] = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]
 
-  it('should correctly generate the third generation', () => {
-    expect(computeNextGeneration(secondGeneration)).toEqual(thirdGeneration)
-  })
+      const expectedResult: cellRange[][] = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]
 
-  it('should correctly generate the fourth generation', () => {
-    expect(computeNextGeneration(thirdGeneration)).toEqual(fourthGeneration)
+      expect(computeNextGeneration(cellValues)).toEqual(expectedResult)
+    })
+
+    it('a cell with 2 or 3 live neighbors lives on to the next generation.', () => {
+      const cellValues: cellRange[][] = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]
+
+      const expectedResult: cellRange[][] = [
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]
+      expect(computeNextGeneration(cellValues)).toEqual(expectedResult)
+    })
+
+    it('a cell with more than 3 live neighbors dies of overcrowding.', () => {
+      const cellValues: cellRange[][] = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]
+
+      const expectedResult: cellRange[][] = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]
+      expect(computeNextGeneration(cellValues)).toEqual(expectedResult)
+    })
+
+    it('an empty cell with exactly 3 live neighbors "comes to life".', () => {
+      const cellValues: cellRange[][] = [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+      ]
+
+      const expectedResult: cellRange[][] = [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+      ]
+
+      expect(computeNextGeneration(cellValues)).toEqual(expectedResult)
+    })
   })
 })
